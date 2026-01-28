62489248d9b2a74b97fb0e51581fcb70c9b5b75d
Mahesh6885 <maheshdmahi740@gmail.com>
2026-01-28 21:36:21 +0530

    Added questions and result page

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from . models import Quiz,Question
+from . models import Quiz,Question,Choice
 
 def quiz_list(request):
     quizes=Quiz.objects.filter(is_active=True)
@@ -13,4 +13,14 @@ def quiz_detail(request,id):
 def start_quiz(request,id):
     quiz=get_object_or_404(Quiz,id=id,is_active=True)
     question=Question.objects.filter(quiz=quiz).prefetch_related('choice_set')
-    return render(request,"quiz/question.html",{'quizs':quiz},{'questions':question})
+    if request.method=="POST":
+        total=question.count()
+        score=0
+        for ques in question:
+            selected_c_id=request.POST.get("question_{ques.id}")
+            if selected_c_id:
+                selected_choice=Choice.objects.get(id=selected_c_id)
+                if selected_choice.is_correct:
+                    score+=1
+        return render(request,"quiz/result.html",{"quiz":quiz,"total":total,"score":score})
+    return render(request,"quiz/question.html",{'quizs':quiz,'questions':question})
